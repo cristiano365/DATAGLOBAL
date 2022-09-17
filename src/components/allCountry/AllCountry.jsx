c2270d155e4909563d5312dc37c3d94e6b9c90f6
Cristiano Vagnoni <54573842+cristiano365@users.noreply.github.com>
2022-09-18 00:01:46 +0200

    First Commit modified

--- src/components/allCountry/AllCountry.jsx
+++ src/components/allCountry/AllCountry.jsx
@@ -1,60 +1,105 @@
 import React, {useState,useEffect} from 'react'
 import { apiURL } from '../utility/api';
 
-const AllCountry = () => {
+import SearchInput from '../search/SearchInput';
+import FilterCountry from '../filterCountry/FilterCountry';
+
+import  {Link} from 'react-router-dom'
+const AllCountry = () => {  
     
     const [countries, setCountries] = useState([])
     const [isLoading, setIsLoading] = useState(true)
     const [error, setError] = useState('')
 
-    const getAllcountries = async()=>{
+    const getAllCountries = async () => {
         try {
-            const res = await fetch(`${apiURL}/all`)
+          const res = await fetch(`${apiURL}/all`);
+    
+          if (!res.ok) throw new Error("Something went wrong!");
+    
+          const data = await res.json();
+          console.log(data);
+          
+          setCountries(data);
+          setIsLoading(false);
 
-            if(!res) throw new Error ('Something went wrong!');
-            
-            const data = await res.json();
-            console.log(data);
-            setCountries(data);
-        
-            setIsLoading(false);
         } catch (error) {
-        setIsLoading(false);
-        setError(error.message);
+          setIsLoading(false);
+          setError(error.message);
         }
-    };
+      };
+    
+      const getCountryByName = async (countryName) => {
+        try {
+          const res = await fetch(`${apiURL}/name/${countryName}`);
+    
+          if (!res.ok) throw new Error("Not found any country, please refresh!");
+    
+          const data = await res.json();
 
-    useEffect( () => {
-        getAllcountries();
-    },[])
-  
+          setCountries(data);
+          setIsLoading(false);
+
+        } catch (error) {
+          setIsLoading(false);
+          setError(error.message);
+        }
+      };
+    
+      const getCountryByRegion = async (regionName) => {
+        try {
+          const res = await fetch(`${apiURL}/region/${regionName}`);
+    
+          if (!res.ok) throw new Error("Failed..........");
+    
+          const data = await res.json();
+
+          setCountries(data);
+          setIsLoading(false);
+
+        } catch (error) {
+          setIsLoading(false);
+          setError(false);
+        }
+      };
+
+      useEffect(() => {
+        getAllCountries();
+      }, []);
+
+
+
     return (
     <div className='all_country_wrapper'>
         <div className='country_top'>
-
+            <div className='search'>
+                <SearchInput onSearch={getCountryByName} />
+            </div>
+            <div className="filter">
+                <FilterCountry onSelect={getCountryByRegion} />
+            </div>  
         </div>
+
         <div className='country_bottom'>
             {isLoading && !error && <h4>Loading.......</h4>}
             {error && !isLoading && <h4>{error}</h4>}
 
             {
-                countries?.map(country=>(
-                    <div className='country_card'>
-                        <div className='country_img'>
-                            <img src={country.flags.png} alt="" />
+                countries?.map(country =>(
+                    <Link to={`/country/${country.name.common}`}>
+                        <div className='country_card'>
+                            <div className='country_img'>
+                                <img src={country.flags.png} alt="" />
+                            </div>
+                            <div className='country_data'>
+                                <h2>{country.name.common}</h2>
+                                <h5>Region: {country.region}</h5>
+                                <h5>Capital: {country.capital} </h5>
+                            </div>    
                         </div>
-                        <div className='country_data'>
-                            <h2>{country.name.common}</h2>
-                            <h5>POPULATION: {country.population}</h5>
-                            <h5>REGION: {country.region}</h5>
-                            <h5>CAPITAL CITY: {country.capital} </h5>
-                        </div>    
-                    </div>
-                ))
-            }
-
+                    </Link>
+                ))}
         </div>
-      
     </div>
   )
 };
